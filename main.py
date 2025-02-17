89d0ecb4c8c3f17d1d15d28e5e212312f7ac9dc4
grzegorzwilk <95700863+grzegorzwilk@users.noreply.github.com>
2025-02-17 13:35:34 +0100

    hello world endpoint test

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/value')
+def value():
+  return jsonify({"value": "Hello, World!"})
 
 if __name__ == '__main__':
   app.run(port=5000)
